4932453ea63e0d694287ee80355f9cfb76a74765
Aulia0103 <aullazzhra86@gmail.com>
2022-11-08 13:08:02 +0700

    first commit

---
+++ resources/views/contents/about.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.index')
+
+@section('content')
+<!-- About Section-->
+<section class="page-section bg-primary text-white mb-0" id="about">
+    <div class="container">
+        <!-- About Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase text-white p-4">About</h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+            <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- About Section Content-->
+        <div class="row">
+            <div class="col-lg-4 ms-auto"><p class="lead">Nama Saya Aulia Az Zahra panggail saja Rara, tempat lahirku di Bandung tanggal 01 Maret 2003.</p></div>
+            <div class="col-lg-4 me-auto"><p class="lead">Saya Melanjutkan Pendidikan di Kampus AMIK Bumi Nusantara Cirebon. sekarang sudah memasuki pertengahan semester 3</p></div>
+        </div>
+            </a>
+        </div>
+    </div>
+</section>
+@endsection
